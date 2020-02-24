59b4d1f74f9b3a07f8a4b19c24ad9d20ea28a077
mrzhou <463913540@qq.com>
2020-02-24 21:19:48 +0800

    初始化样式

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{mix('css/app.css')}}">
+  <title>@yield('title','weibo App')</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a href="/" class="navbar-brand">Weibo App</a>
+    <ul class="navbar-nav justify-content-end">
+      <li class="nav-item">
+        <a href="/help" class="nav-link">帮助</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">登陆</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="container">
+  @yield('content')
+</div>
+
+
+</body>
+</html>
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('帮助页面')
+@section('content')
+  <h1>帮助</h1>
+@stop
