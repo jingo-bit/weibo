b4ae694a79022ef6784951f0c75b5a476fa258cc
mrzhou <463913540@qq.com>
2020-02-25 14:54:35 +0800

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -5,6 +5,6 @@
     <h1>Hello laravel</h1>
     <p class="lead">你现在所看到的是<a href="https://learnku.com/docs/laravel/5.5/installation/1282">laravel 入门教程</a></p>
     <p>一切将从这里开始</p>
-    <p class="btn btn-lg btn-success"><a href="#" role="button" style="color: white">现在注册</a></p>
+    <p class="btn btn-lg btn-success"><a href="{{ route('signup') }}" role="button" style="color: white">现在注册</a></p>
   </div>
 @stop
