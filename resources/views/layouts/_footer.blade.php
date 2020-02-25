3d3ac5751e46f37ba04ff62c1c50263e0bf47fc9
mrzhou <463913540@qq.com>
2020-02-25 14:45:50 +0800

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,3 +1,10 @@
 <footer class="footer">
-  <span>刻意联系，每日精进</span>
+  <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+  <a href="https://learnku.com/laravel/courses" target=_blank>
+    刻意练习，每日精进
+  </a>
+
+  <div>
+    <a href="{{ route('about') }}" >关于</a>
+  </div>
 </footer>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
   <div class="jumbotron">
     <h1>Hello laravel</h1>
-    <p class="lead">你现在所看到的是<a href="#">laravel 入门教程</a></p>
+    <p class="lead">你现在所看到的是<a href="https://learnku.com/docs/laravel/5.5/installation/1282">laravel 入门教程</a></p>
     <p>一切将从这里开始</p>
     <p class="btn btn-lg btn-success"><a href="#" role="button" style="color: white">现在注册</a></p>
   </div>
