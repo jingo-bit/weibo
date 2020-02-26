f8c74c8e059712235560287121b3f1a881920009
mrzhou <463913540@qq.com>
2020-02-26 09:53:08 +0800

    updated datebase configuration

---
+++ app/helpers.php
@@ -0,0 +1,24 @@
+<?php
+
+function get_db_config()
+{
+    if (getenv('IS_IN_HEROKU')) {
+        $url = parse_url(getenv("DATABASE_URL"));
+
+        return $db_config = [
+            'connection' => 'pgsql',
+            'host' => $url["host"],
+            'database'  => substr($url["postgres://zazrpwexwiyuxd:74c007700a84249aad39a5d6c3fc1756a73d09e8d11fc7216e2bd42841363ccf@ec2-18-210-51-239.compute-1.amazonaws.com:5432/d7eo9gkt1ct3bq"], 1),
+            'username'  => $url["user"],
+            'password'  => $url["pass"],
+        ];
+    } else {
+        return $db_config = [
+            'connection' => env('DB_CONNECTION', 'mysql'),
+            'host' => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
+        ];
+    }
+}
