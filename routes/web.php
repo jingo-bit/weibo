49c632bbc60752e887bd2d7438fada23d766b266
mrzhou <463913540@qq.com>
2020-02-28 11:05:45 +0800

    关注和粉丝列表页面

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,7 @@ Route::delete('logout','SessionController@destroy')->name('logout');
 
 
 Route::resource('statuses','StatusesController',['only' =>['store','destroy']]);
+
+
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
